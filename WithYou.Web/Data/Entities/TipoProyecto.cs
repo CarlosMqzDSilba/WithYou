8e4350d22747040f2cb4b0a317c5eab9ff6f07ec
Carlos Alberto Marquez Silva <carlosalberto.marquezs@my.uvm.edu.mx>
2020-04-20 15:59:35 -0500

    IEntity TipoProyecto Estado de la republica Genero Lider

---
+++ WithYou.Web/Data/Entities/TipoProyecto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WithYou.Web.Data.Entities
+{
+    public class TipoProyecto:IEntity
+    {
+        public int Id { get; set; }
+    }
+}
